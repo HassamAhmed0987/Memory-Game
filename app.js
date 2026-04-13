f3ca8f5f6b977c48a8548671e9756e9223a19155
HassamAhmed0987 <hassamansari0987@gmail.com>
2026-04-13 16:42:25 +0500

    moves check

--- app.js
+++ app.js
@@ -1,10 +1,35 @@
 const emojis = ['🍔', '🍔', '🍧', '🍧', '🍕', '🍕', '🍉', '🍉', '🍗', '🍗', '🍓', '🍓', '🍒', '🍒', '🥧', '🥧', '🌺', '🌺', '🍡', '🍡']
 let flippedCards = []
 let matchedCards = 0
+let movesCount = 20
+let timerSecond = 60
+let timerminute = 1
 
 const container = document.querySelector("#grid-game")
 const startBtn = document.querySelector(".start-game")
+const movesElement = document.querySelector(".moves-count")
+const timerSecELe = document.querySelector(".timer-second")
+const timerMintELe = document.querySelector(".timer-minute")
 
+
+// let timer = setInterval(() => {
+//     timerSecond--
+//     if (timerSecond === 0 && timerminute !== 0) {
+//         timerSecond = 60
+//         timerminute--
+//     }
+//     if (timerminute > 0) {
+//         // console.log("hello");
+//         // console.log(timerminute);
+//     } else {
+//         alert('Time Over! Try Again!')
+//         clearInterval(timer)
+//     }
+//     // console.log(timerminute,timerSecond);
+//     timerSecELe.textContent = timerSecond
+//     timerMintELe.textContent = timerminute
+// }, 1000)
+movesElement.textContent = movesCount
 startBtn.addEventListener("click", restartGame)
 
 function startGame() {
@@ -24,27 +49,67 @@ function startGame() {
         container.appendChild(card)
         card.addEventListener("click", flipcard)
     })
+    // timerSecond = 60
+    // timerminute = 1
+    // let timer = setInterval(() => {
+        // if(timerminute !== -1){
+        //     timerSecond--
+        //     if (timerSecond === 0) {
+        //         timerSecond = 60
+        //         timerminute--
+        //     }
+        //     // if (timerminute > -1) { 
+        //     // } 
+        //     // else {
+        //     //     alert('Time Over! Try Again!')
+        //     //     clearInterval(timer)
+        //     //     restartGame()
+        //     // }
+        // } else{
+        //     alert('Time Over')
+        //     restartGame()
+        // }
+            // console.log(timerminute,timerSecond);
+    //     timerSecELe.textContent = timerSecond
+    //     timerMintELe.textContent = timerminute
+    // }, 1000)
+    movesCount = 20
+    movesElement.textContent = movesCount
 }
 function flipcard() {
-    if(flippedCards.length < 2 && !this.classList.contains("flipped") && !this.classList.contains('matched')){
+    if (flippedCards.length < 2 && !this.classList.contains("flipped") && !this.classList.contains('matched')) {
         this.classList.add("flipped")
         flippedCards.push(this)
-    } 
-    if(flippedCards.length === 2){
+    }
+    if (flippedCards.length === 2) {
         setTimeout(matchCheck, 600)
     }
 }
 function matchCheck() {
     const [card1, card2] = flippedCards
-
-    if(card1.dataset.name === card2.dataset.name){
+    movesCount--
+    movesElement.textContent = movesCount
+    if (movesCount === 0) {
+        alert("Game Over! Try Again")
+        movesCount = 20
+        startGame()
+    }
+    if (card1.dataset.name === card2.dataset.name) {
         card1.classList.add('matched')
+        setTimeout(() => {
+            card1.classList.add('fly')
+            card1.classList.add('gaiab')
+        }, 500)
         card2.classList.add('matched')
+        setTimeout(() => {
+            card2.classList.add('fly')
+            card2.classList.add('gaiab')
+        }, 500)
         matchedCards += 2
-        if(matchedCards === emojis.length){
+        if (matchedCards === emojis.length) {
             alert("You Win")
         }
-    }else{
+    } else {
         card1.classList.remove('flipped')
         card2.classList.remove('flipped')
     }
